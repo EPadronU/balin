467ab542789334db4c522beaf624ccd5d9aa9681
Edinson E. Padrón Urdaneta <edinson.padron@posteo.net>
2018-07-24 12:35:22 -0400

    Reformat build.gradle.kts file

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,28 +27,28 @@ val testNgVersion: String by project
 
 /* Build script's setup ******************************************************/
 buildscript {
-  repositories {
-    jcenter()
-  }
+    repositories {
+        jcenter()
+    }
 
-  dependencies {
-    classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.3")
-    classpath("io.qameta.allure:allure-gradle:2.5")
-  }
+    dependencies {
+        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.3")
+        classpath("io.qameta.allure:allure-gradle:2.5")
+    }
 }
 /* ***************************************************************************/
 
 /* Plugins *******************************************************************/
 plugins {
-  kotlin("jvm") version "1.2.51"
-  id("io.gitlab.arturbosch.detekt") version "1.0.0.RC7-3"
+    kotlin("jvm") version "1.2.51"
+    id("io.gitlab.arturbosch.detekt") version "1.0.0.RC7-3"
 }
 
 apply {
-  plugin("com.jfrog.bintray")
-  plugin("io.qameta.allure")
-  plugin("maven")
-  plugin("maven-publish")
+    plugin("com.jfrog.bintray")
+    plugin("io.qameta.allure")
+    plugin("maven")
+    plugin("maven-publish")
 }
 /* ***************************************************************************/
 
@@ -59,104 +59,104 @@ version = projectVersion
 
 /* Dependencies **************************************************************/
 dependencies {
-  compile(kotlin("$kotlinStdLib"))
-  compile(kotlin("reflect"))
-  compile("org.slf4j:slf4j-api:$slf4jVersion")
-  compile("org.seleniumhq.selenium:selenium-api:$seleniumVersion")
-  compile("org.seleniumhq.selenium:selenium-firefox-driver:$seleniumVersion")
-  compile("org.seleniumhq.selenium:selenium-support:$seleniumVersion")
+    compile(kotlin(kotlinStdLib))
+    compile(kotlin("reflect"))
+    compile("org.slf4j:slf4j-api:$slf4jVersion")
+    compile("org.seleniumhq.selenium:selenium-api:$seleniumVersion")
+    compile("org.seleniumhq.selenium:selenium-firefox-driver:$seleniumVersion")
+    compile("org.seleniumhq.selenium:selenium-support:$seleniumVersion")
 
-  testCompile("org.testng:testng:$testNgVersion")
-  testCompile("org.seleniumhq.selenium:htmlunit-driver:$htmlUnitDriverVersion")
+    testCompile("org.testng:testng:$testNgVersion")
+    testCompile("org.seleniumhq.selenium:htmlunit-driver:$htmlUnitDriverVersion")
 }
 
 repositories {
-  jcenter()
+    jcenter()
 }
 /* ***************************************************************************/
 
 /* Allure's setup **********************************************************/
 configure<AllureExtension> {
-  autoconfigure = false
+    autoconfigure = false
 
-  version = allureVersion
-
-  useTestNG = closureOf<TestNGConfig> {
     version = allureVersion
-  }
+
+    useTestNG = closureOf<TestNGConfig> {
+        version = allureVersion
+    }
 }
 /* ***************************************************************************/
 
 /* Detekt's setup ************************************************************/
 detekt {
-  version = "1.0.0.RC7-3"
+    version = "1.0.0.RC7-3"
 
-  profile("main", Action {
-    input = "src/main/kotlin"
-    filters = ".*/resources/.*"
-    output = "detekt-reports"
-    outputName = "detekt-report"
-    baseline = "reports/baseline.xml"
-  })
+    profile("main", Action {
+        input = "src/main/kotlin"
+        filters = ".*/resources/.*"
+        output = "detekt-reports"
+        outputName = "detekt-report"
+        baseline = "reports/baseline.xml"
+    })
 }
 /* ***************************************************************************/
 
 /* Publication setup *********************************************************/
 val sourcesJarTask = task<Jar>("sourceJar") {
-  from(java.sourceSets["main"].allSource)
+    from(java.sourceSets["main"].allSource)
 
-  classifier = "sources"
+    classifier = "sources"
 }
 
 configure<PublishingExtension> {
-  publications {
-    create<MavenPublication>("JCenterPublication") {
-      from(components["java"])
-      artifact(sourcesJarTask)
+    publications {
+        create<MavenPublication>("JCenterPublication") {
+            from(components["java"])
+            artifact(sourcesJarTask)
 
-      groupId = projectGroup
-      artifactId = projectName
-      version = projectVersion
+            groupId = projectGroup
+            artifactId = projectName
+            version = projectVersion
+        }
     }
-  }
 }
 
 configure<BintrayExtension> {
-  user = System.getenv("JFROG_USER")
-  key = System.getenv("JFROG_KEY")
+    user = System.getenv("JFROG_USER")
+    key = System.getenv("JFROG_KEY")
 
-  setPublications("JCenterPublication")
+    setPublications("JCenterPublication")
 
-  pkg.apply {
-    repo = "maven"
-    name = projectName
-    desc = projectDescription
-    vcsUrl = projectRepository
-    publish = true
-    setLabels("kotlin", "selenium", "web", "geb", "automation")
-    setLicenses("Apache-2.0")
-    publicDownloadNumbers = true
-    version.apply {
-      name = projectVersion
-      desc = projectDescription
-      released = Date().toString()
+    pkg.apply {
+        repo = "maven"
+        name = projectName
+        desc = projectDescription
+        vcsUrl = projectRepository
+        publish = true
+        setLabels("kotlin", "selenium", "web", "geb", "automation")
+        setLicenses("Apache-2.0")
+        publicDownloadNumbers = true
+        version.apply {
+            name = projectVersion
+            desc = projectDescription
+            released = Date().toString()
+        }
     }
-  }
 }
 /* ***************************************************************************/
 
 /* Test task configuration ***************************************************/
 tasks.withType(Test::class.java).all {
-  ignoreFailures = true
+    ignoreFailures = true
 
-  useTestNG()
+    useTestNG()
 }
 /* ***************************************************************************/
 
 /* JVM attributes ************************************************************/
 tasks.withType(KotlinCompile::class.java).all {
-  kotlinOptions {
-    jvmTarget = jvmTargetVersion
-  }
+    kotlinOptions {
+        jvmTarget = jvmTargetVersion
+    }
 }
 /* ***************************************************************************/
